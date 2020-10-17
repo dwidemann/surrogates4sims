a76e00cd6c213ee83518eb72a66b20b4dff5cf6c
Brian Bartoldson <bartoldson1@llnl.gov>
2020-10-16 17:41:38 -0700

    add ouput files to .gitignore, add script version of 09 manta notebook, add bash driver for script, add output of nbdev on new 09 script notebook

---
+++ run_w_z_search.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+w=("1" "5" "10" "20" "30" "50" "100" "150")
+z=("8" "16" "32" "64")
+for k in "${w[@]}"; do
+        for p in "${z[@]}"; do
+                python -m surrogates4sims.full_svd_surrogate_script --window $k --numComponents $p --gpu_ids 0
+        done
+done
