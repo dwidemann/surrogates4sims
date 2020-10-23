82c5198c2344e27c0641d5c19611edd413c010d5
Brian Bartoldson <bartoldson1@llnl.gov>
2020-10-23 11:51:26 -0700

    add 128 and 256 to z sweep

--- run_w_z_search.sh
+++ run_w_z_search.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 w=("1" "5" "10" "20" "30" "50" "100" "150")
-z=("8" "16" "32" "64")
+z=("8" "16" "32" "64" "128" "256")
 for k in "${w[@]}"; do
         for p in "${z[@]}"; do
                 python -m surrogates4sims.full_svd_surrogate_script --window $k --numComponents $p --gpu_ids 0
